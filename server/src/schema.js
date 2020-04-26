1e8f96077d22236b1141b1a468bb896f40eee72f
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-04-26 18:00:00 +0530

    added server

---
+++ server/src/schema.js
@@ -0,0 +1,54 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Launch {
+        id: ID!
+        site: String
+        mission: Mission
+        rocket: Rocket
+        isBooked: Boolean!
+    }
+    type Rocket {
+        id: ID!
+        name: String
+        type: String
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        trips: [Launch]!
+    }
+
+    type Mission {
+        name: String
+        missionPatch(mission: String, size: PatchSize): String
+    }
+
+    enum PatchSize {
+        SMALL
+        LARGE
+    }
+    type Query {
+        launches(pageSize: Int, after: String): LaunchConnection!
+        launch(id: ID!): Launch
+        me: User
+    }
+    type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): String # login token
+    }
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
+    }
+    type LaunchConnection { # add this below the Query type as an additional type.
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
+    }
+`;
+
+module.exports = typeDefs;
